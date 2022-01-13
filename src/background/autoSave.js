2ea65e5695ab07504d7827403e99360b0383b01a
Manuel [tennox] <2084639+tennox@users.noreply.github.com>
2022-01-13 06:17:04 +0100

    Fix: clearActiveSessionOnBrowserClose saves wrong session
    the autoSave would save the currently open session (which is e.g. empty) instead of saving the temp one created when the browser was exited.

--- src/background/autoSave.js
+++ src/background/autoSave.js
@@ -152,9 +152,9 @@ export const autoSaveWhenWindowClose = async removedWindowId => {
 export const autoSaveWhenExitBrowser = async () => {
   const tempSessions = await getSessionsByTag("temp");
   if (!tempSessions[0]) return;
-  log.info(logDir, "autoSaveWhenExitBrowser()");
-
   let session = tempSessions[0];
+  log.info(logDir, "autoSaveWhenExitBrowser()", session);
+
   if (!getSettings("useTabTitleforAutoSave"))
     session.name = browser.i18n.getMessage("browserExitSessionName");
   session.tag = ["browserExit"];
@@ -162,12 +162,12 @@ export const autoSaveWhenExitBrowser = async () => {
   session.id = uuidv4();
 
   if (getSettings('keepTrackOfActiveSession')) {
-    updateActiveSession(session);
+    await updateActiveSession(session);
   }
 
   // Clears the active session upon browser close if such setting is set
   if (getSettings('clearActiveSessionOnBrowserClose')) {
-    await setActiveSession(null);
+    await setActiveSession(null, null, true /* session is saved above */);
   }
 
   await saveSession(session);
